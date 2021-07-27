ee8b89d71262c45672db887af90cba973f767486
Lorenzo Calvello <lo.cal_y@yahoo.com>
2021-07-27 19:50:44 +0200

    setting up jwt token authentication

--- core/schema.py
+++ core/schema.py
@@ -1,11 +1,14 @@
 import graphene
 from graphene.types import schema
 import accounts.schema
+import graphql_jwt
 
 class RootQuery(graphene.ObjectType):
   helloQuery = graphene.String(default_value='Hello Query!')
 
 class RootMutation(accounts.schema.Mutation, graphene.ObjectType):
-  helloMutation = graphene.String(default_value='Hello Mutation!')
+  token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+  verify_token = graphql_jwt.Verify.Field()
+  refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=RootQuery, mutation=RootMutation)
